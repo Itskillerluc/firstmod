007e153adb6da8496f8290ce0c3dff605603f22b
Itskillerluc <lvgraef@zeelandnet.nl>
2021-10-21 16:51:48 +0200

    commit

--- src/main/java/com/itskillerluc/firstmod/world/gen/ModTreeGen.java
+++ src/main/java/com/itskillerluc/firstmod/world/gen/ModTreeGen.java
@@ -6,26 +6,26 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
+import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 import java.util.function.Supplier;
 
 public class ModTreeGen {
 
     public static void genTrees(final BiomeLoadingEvent event){
-        MagicalTree magicalTree = new MagicalTree();
-        Random random = new Random();
 
         RegistryKey<Biome> key = RegistryKey.create(Registry.BIOME_REGISTRY, event.getName());
         Set<BiomeDictionary.Type> types = BiomeDictionary.getTypes(key);
 
         if(types.contains(BiomeDictionary.Type.MAGICAL)){
             List<Supplier<ConfiguredFeature<?, ?>>> base = event.getGeneration().getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION);
-            base.add(() -> ModConfiguredFeatures.MAGICAL_TREE.)
+            base.add(() -> ModConfiguredFeatures.MAGICAL_TREE.decorated(Features.Placements.HEIGHTMAP).decorated(Placement.COUNT_EXTRA.configured(new AtSurfaceWithExtraConfig(1,0.01f, 0))));
         }
     }
 
